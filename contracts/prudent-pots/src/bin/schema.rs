2feb9087a7d8a8674fec125da5c03f784ff8856f
magiodev <31893902+magiodev@users.noreply.github.com>
2024-03-25 12:49:51 +0100

    contract rename and minor changes

--- contracts/prudent-pots/src/bin/schema.rs
+++ contracts/prudent-pots/src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use prudent_pot::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use prudent_pots::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
